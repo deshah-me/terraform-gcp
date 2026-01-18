91cf7d928a13ce2a945f46c394d977e296126392
deshah-me <121696578+deshah-me@users.noreply.github.com>
2026-01-18 15:47:32 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "7.16.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+  project     = "us-con-gcp-sbx-dep0019-081424"
+  region      = "us-east1"
+  zone        = "us-east1-b"
+  credentials = "./keys.json"
+}
